39d03e077923e79ad8f13f9a83b0fcfcd3cf0eeb
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-06-30 20:45:00 -0700

    seeded tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
-USE company_db
+USE company_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -12,7 +12,7 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    FOREIGN KEY(department_id)
+    FOREIGN KEY(department_id),
     REFERENCES department(id)
     --potentially on delete set null
 );
@@ -21,8 +21,8 @@ CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    FOREIGN KEY(role_id)
-    REFERENCES(role.id)
+    FOREIGN KEY(role_id),
+    REFERENCES(role.id),
     FOREIGN KEY(manager_id),
     REFERENCES(employee.id)
 );
