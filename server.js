08c8e44db85fc8b6b0716d20f184a45bd4ee067f
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-01 17:43:48 -0700

    fixed file structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+require("dotenv")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,3 +9,23 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: DB_USER,
+      // TODO: Add MySQL password here
+      password: DB_PASSWORD,
+      database: DB_NAME,
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
